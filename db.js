c7bcb0fd2df6f3bfd87e3821e7f5abc9a3873cb3
EricZitongZhou <zitongzhou1999@gmail.com>
2020-04-21 17:20:53 -0400

    update

--- db.js
+++ db.js
@@ -40,15 +40,18 @@ let dbconf = 'mongodb://localhost/game';
 
 // if we're in PRODUCTION mode, then read the configration from a file
 // use blocking file io to do this...
-const fs = require('fs');
-const path = require('path');
-const fn = path.join(__dirname, 'config.json');
-const data = fs.readFileSync(fn);
 
-// our configuration file will be in json, so parse it and set the
-// conenction string appropriately!
-const conf = JSON.parse(data);
-dbconf = conf.dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION'){
+  const fs = require('fs');
+  const path = require('path');
+  const fn = path.join(__dirname, 'config.json');
+  const data = fs.readFileSync(fn);
+
+  // our configuration file will be in json, so parse it and set the
+  // conenction string appropriately!
+  const conf = JSON.parse(data);
+  dbconf = conf.dbconf;
+}
 
 
 mongoose.connect(dbconf, mongooseOptions);
