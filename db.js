9fe9f24987370b408461ace827304322db2f056c
EricZitongZhou <zitongzhou1999@gmail.com>
2020-04-21 17:02:10 -0400

    db path

--- db.js
+++ db.js
@@ -33,7 +33,25 @@ const CommentSchema = new mongoose.Schema({
 mongoose.model('User', UserSchema); // collection name will be articles
 mongoose.model('Board', BoardSchema);
 mongoose.model('Comment', CommentSchema);
-mongoose.connect('mongodb://localhost/game', mongooseOptions);
 
-console.log('Schema Registered\n');
+
+
+let dbconf = 'mongodb://localhost/game';
+
+// if we're in PRODUCTION mode, then read the configration from a file
+// use blocking file io to do this...
+const fs = require('fs');
+const path = require('path');
+const fn = path.join(__dirname, 'config.json');
+const data = fs.readFileSync(fn);
+
+// our configuration file will be in json, so parse it and set the
+// conenction string appropriately!
+const conf = JSON.parse(data);
+dbconf = conf.dbconf;
+
+
+mongoose.connect(dbconf, mongooseOptions);
+
+console.log(dbconf, 'Schema Registered\n');
 
